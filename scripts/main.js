9f5259455b1cba7242563a3ed603671cfa298f29
Érick Lúcio <104475806+ericklucioh@users.noreply.github.com>
2023-10-24 22:42:58 -0300

    Finalização

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,14 @@ let buttons = "";
 for (let i = 0; i < db.contents.length; i++) {
     buttons +=`
     <a id="${db.contents[i].id}" class="button" href="${db.contents[i].link}" target="_blank">
-        <svg>
-            <title>
-                ${db.contents[i].name}
-            </title>
-            <path fill="#000" d="${db.contents[i].img}">
-            </path>
-        </svg>
-        <h2>
-            ${db.contents[i].name}
-        </h2>
+        <div class="intern">
+            ${db.contents[i].img}
+            <h2>
+                ${db.contents[i].name} 
+            </h2>
+        </div>
     </a>`;
+
 }
 
 const display = document.getElementById("buttons");
