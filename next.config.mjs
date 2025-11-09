a404fa7524ed7327549659e5917641c66ee1881e
ericklucioh <erickrealoficial@gmail.com>
2025-11-09 20:32:08 -0300

    [main] fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',          // 🔥 ISSO AQUI É CRUCIAL
+  trailingSlash: true,
+  images: {
+    unoptimized: true       // Necessário para export estático
+  },
+}
 
 export default nextConfig;
