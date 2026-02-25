28d6be17a99662b6ef01c2a0ab22ac27daaa3be2
ericklucioh <erickrealoficial@gmail.com>
2026-02-25 19:42:43 -0300

    [main] fix page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState, FormEvent } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Senha</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">Entrar</button>
+    </form>
+  );
+}
