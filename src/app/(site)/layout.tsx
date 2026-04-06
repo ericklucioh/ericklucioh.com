b4d4749b5fdbf54e1970ac2792fc7045a176cbb2
ericklucioh <erickrealoficial@gmail.com>
2026-04-05 22:14:01 -0300

    [main] about

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,17 @@
+import Menu from "@/components/layout/Menu";
+
+export default function SiteLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Menu
+        buttons={[
+          { label: "home", href: "/linktree" },
+          { label: "sobre", href: "/about" },
+          { label: "blog", href: "/blog" },
+          { label: "contato", href: "/contato" },
+        ]}
+      />
+      <div style={{ paddingTop: "var(--header-h)" }}>{children}</div>
+    </>
+  );
+}
